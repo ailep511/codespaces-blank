06f908d087d32771954098e2c962d9a967d8930b
ailep511 <162311008+ailep511@users.noreply.github.com>
2025-10-29 10:01:16 -0300

    Add another question

--- questions/index.ts
+++ questions/index.ts
@@ -3,10 +3,11 @@ import questions01 from "./questions_01.json"
 import questions02 from "./questions_02.json"
 import questions03 from "./questions_03.json"
 import questions04 from "./questions_04.json"
+import questions04 from "./questions_05.json"
 
 // Export all question files as an array
 // export const questionFiles = [questions01, questions02, questions03, questions04]
-export const questionFiles = [questions04]
+export const questionFiles = [questions04, questions05]
 
 // Function to get a random question file
 export function getRandomQuestionFile() {
